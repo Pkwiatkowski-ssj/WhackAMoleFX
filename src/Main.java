9e79d6593ea1a1b7c5b54c89574d047062ff0d53
pkwiatkowski-ssj <pkwiatkowski@quinnipiac.edu>
2025-04-09 15:03:48 -0400

    Created working whack me pad

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,24 @@
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button("Whack Me!");
+        btn.setOnAction(e -> System.out.println("WHACK!"));
+
+        StackPane root = new StackPane(btn);
+        Scene scene = new Scene(root, 300, 250);
+
+        primaryStage.setTitle("Whack-a-Mole");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
